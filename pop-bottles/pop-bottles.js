aab2eca44487c2e0f951e04c8ae9bea1ff7c3dfb
Matt Cook <mcc1985@gmail.com>
2019-02-09 01:04:10 +0000

    Full tally poppin bottles

--- pop-bottles/pop-bottles.js
+++ pop-bottles/pop-bottles.js
@@ -8,6 +8,8 @@ function poppinBottles(cash) {
 
   var tally = {
     'Total Bottles': currentBottles,
+    'Remaining Empties': 0,
+    'Remaining Caps': 0,
     'Earned Bottles': {
       Bottles: 0,
       Caps: 0
@@ -16,77 +18,21 @@ function poppinBottles(cash) {
   };
 
 
-  while (currentBottles > 0 || numEmpties >= 2 || numCaps >= 4) {
+  while (currentBottles > 0 || tally['Remaining Empties'] >= 2 || tally['Remaining Caps'] >= 4) {
     //Drink Pops
-    numEmpties += currentBottles;
-    numCaps += currentBottles;
+    tally['Remaining Empties'] += currentBottles;
+    tally['Remaining Caps'] += currentBottles;
     currentBottles = 0;
 
     //Trade in
-    currentBottles = Math.floor(numEmpties / 2) + Math.floor(numCaps / 4);
-    tally['Earned Bottles'].Bottles += Math.floor(numEmpties / 2);
-    tally['Earned Bottles'].Caps += Math.floor(numCaps / 4);
-    numEmpties = numEmpties % 2;
-    numCaps = numCaps % 4;
+    currentBottles = Math.floor(tally['Remaining Empties'] / 2) + Math.floor(tally['Remaining Caps'] / 4);
+    tally['Earned Bottles'].Bottles += Math.floor(tally['Remaining Empties'] / 2);
+    tally['Earned Bottles'].Caps += Math.floor(tally['Remaining Caps'] / 4);
+    tally['Remaining Empties'] = tally['Remaining Empties'] % 2;
+    tally['Remaining Caps'] = tally['Remaining Caps'] % 4;
     tally['Total Bottles'] += currentBottles;
   }
   return tally;
 }
 
 console.log(poppinBottles(cash));
-
-
-/*
-10 b 10t
-0b  10e 10c
-
-5b 0e
-7b 2c
-
-17t
-
-0b 7e 9c
-
-3b 1e
-5b 1c
-
-22t
-
-0b 6e 6c
-3b 0e
-4b 2c
-
-26t
-
-0b 4e 6c
-2b 0e
-3b 2c
-
-29t
-
-0b 3e 5c
-1b 1e
-2b 1c
-
-31t
-
-0b 3e 3c
-1b 1e 3c
-
-32
-
-0b 2e 4c
-1b 0e
-2b 0c
-
-34t
-
-0b 2e 2c
-1b 0e 2c
-
-35t
-
-0b 1e 3c
-
-
-*/
